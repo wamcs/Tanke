fbd3025ef505872f8d26923a7051f2bda9776f6e
Xiaojuanmao <daque@hustunique.com>
2016-05-19 14:47:31 +0800

    adjust doc

--- app/src/main/java/com/lptiyu/tanke/MainActivity.java
+++ app/src/main/java/com/lptiyu/tanke/MainActivity.java
@@ -1,31 +1,25 @@
 package com.lptiyu.tanke;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
 
-import com.baidu.mapapi.map.BaiduMap;
-import com.baidu.mapapi.map.TextureMapView;
-import com.lptiyu.tanke.trace.bean.HistoryTrackData;
-import com.lptiyu.tanke.trace.history.HistoryTrackCallback;
-import com.lptiyu.tanke.trace.history.HistoryTrackHelper;
-import com.lptiyu.tanke.trace.history.IHistoryTrackHelper;
-import com.lptiyu.tanke.trace.realtime.IRealTimeTrackHelper;
-import com.lptiyu.tanke.trace.realtime.RealTimeTrackCallback;
-import com.lptiyu.tanke.trace.realtime.RealTimeTrackHelper;
-import com.lptiyu.tanke.trace.tracing.ITracingHelper;
-import com.lptiyu.tanke.trace.tracing.TracingCallback;
-import com.lptiyu.tanke.trace.tracing.TracingHelper;
+import com.lptiyu.tanke.utils.PermissionUtil;
 import com.lptiyu.tanke.utils.ToastUtil;
 import com.lptiyu.zxinglib.android.CaptureActivity;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
+
+  private static final int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +31,35 @@ public class MainActivity extends AppCompatActivity {
 
   @OnClick(R.id.scanner)
   public void onClick() {
-    startActivity(new Intent(this, CaptureActivity.class));
+    PermissionUtil.showCameraWithCheck(this);
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    switch (requestCode) {
+      case REQUEST_CODE_ASK_PERMISSIONS:
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+          // Permission Granted
+          startActivity(new Intent(this, CaptureActivity.class));
+        } else {
+          // Permission Denied
+          ToastUtil.TextToast("Camera Denied");
+        }
+        break;
+      default:
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+  }
+
+  @TargetApi(23)
+  public void requestCameraPermission() {
+    int hasCameraPermission = checkSelfPermission(Manifest.permission.CAMERA);
+    if (hasCameraPermission != PackageManager.PERMISSION_GRANTED) {
+      requestPermissions(new String[]{Manifest.permission.CAMERA},
+          REQUEST_CODE_ASK_PERMISSIONS);
+    } else {
+      startActivity(new Intent(this, CaptureActivity.class));
+    }
   }
 
 }
