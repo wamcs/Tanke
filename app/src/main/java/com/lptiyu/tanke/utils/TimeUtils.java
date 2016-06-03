d33e97638e516d8d15175401822f5b6e023c14bd
Danxiong Lei <danxionglei@foxmail.com>
2016-06-03 14:04:00 +0800

    game detail page over

--- app/src/main/java/com/lptiyu/tanke/utils/TimeUtils.java
+++ app/src/main/java/com/lptiyu/tanke/utils/TimeUtils.java
@@ -1,8 +1,13 @@
 package com.lptiyu.tanke.utils;
 
+import android.content.Context;
+
+import com.lptiyu.tanke.R;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -33,7 +38,7 @@ public class TimeUtils {
   }
 
   /**
-   * @param time  was yyyy-MM-dd or HH:mm:ss
+   * @param time was HH:mm:ss
    */
   public static Date parseTime(String time) {
     try {
@@ -43,6 +48,40 @@ public class TimeUtils {
       return null;
     }
   }
+
+  public static String parseTime(final Context context,
+                                 String startDate, String endDate,
+                                 String startTime, String endTime) {
+    String result;
+    Calendar calendar = Calendar.getInstance();
+
+    Date date = TimeUtils.parseDate(startDate);
+    if (date == null) {
+      result = "";
+    } else {
+      calendar.setTime(date);
+      int _startMonth = calendar.get(Calendar.MONTH);
+      int _startDate = calendar.get(Calendar.DATE);
+      date = TimeUtils.parseDate(endDate);
+      calendar.setTime(date);
+      int _endMonth = calendar.get(Calendar.MONTH);
+      int _endDate = calendar.get(Calendar.DATE);
+      result = String.format(Locale.CHINA, context.getString(R.string.main_page_date_format_pattern),
+          _startMonth, _startDate, _endMonth, _endDate);
+    }
+
+    Date time = TimeUtils.parseTime(startTime);
+    if (time == null) {
+      result += context.getString(R.string.main_page_forever);
+    } else {
+      result += TimeUtils.formatHourMinute(startTime);
+      result += "-";
+      result += TimeUtils.formatHourMinute(endTime);
+    }
+
+    return result;
+  }
+
 
   public static String formatHourMinute(String content) {
     Date parsed = parseTime(content);
