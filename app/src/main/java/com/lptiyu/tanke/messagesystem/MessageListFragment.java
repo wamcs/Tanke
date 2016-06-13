f13511ebcb35abfd0f0f9ebd7a9f14a7464cb2ce
wamcs <kaili@hustunique.com>
2016-06-14 01:47:45 +0800

    push message has bug

---
+++ app/src/main/java/com/lptiyu/tanke/messagesystem/MessageListFragment.java
@@ -0,0 +1,53 @@
+package com.lptiyu.tanke.messagesystem;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.lptiyu.tanke.MainActivityController;
+import com.lptiyu.tanke.R;
+import com.lptiyu.tanke.base.controller.FragmentController;
+import com.lptiyu.tanke.base.ui.BaseFragment;
+import com.lptiyu.tanke.messagesystem.adpater.MessageListAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * author:wamcs
+ * date:2016/6/9
+ * email:kaili@hustunique.com
+ */
+public class MessageListFragment extends BaseFragment {
+
+    @BindView(R.id.message_list_recycler_view)
+    RecyclerView mRecyclerView;
+
+    private MessageListAdapter adapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = fromResLayout(inflater, container, R.layout.fragment_message_list);
+        ButterKnife.bind(this, view);
+        init();
+        return view;
+    }
+
+    private void init(){
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.setAdapter(adapter = new MessageListAdapter(getContext()));
+    }
+
+
+    @Override
+    public FragmentController getController() {
+        return null;
+    }
+}
