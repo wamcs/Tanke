b80237824ca1f17180a248ef961715b8c5407e18
wamcs <kaili@hustunique.com>
2016-07-20 16:10:22 +0800

    commit

--- app/src/main/java/com/lptiyu/tanke/activities/baidumapmode/GameMapShowController.java
+++ app/src/main/java/com/lptiyu/tanke/activities/baidumapmode/GameMapShowController.java
@@ -2,8 +2,17 @@ package com.lptiyu.tanke.activities.baidumapmode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.baidu.mapapi.map.TextureMapView;
+import com.lptiyu.tanke.R;
 import com.lptiyu.tanke.base.controller.ActivityController;
+import com.lptiyu.tanke.gameplaying.assist.MapHelper;
+import com.lptiyu.tanke.widget.TickView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * author:wamcs
@@ -13,9 +22,47 @@ import com.lptiyu.tanke.base.controller.ActivityController;
 public class GameMapShowController extends ActivityController {
 
 
+  @BindView(R.id.map_view)
+  TextureMapView mapView;
+  @BindView(R.id.tick_view)
+  TickView mTickView;
+  @BindView(R.id.zoom_in)
+  ImageView mZoomIn;
+  @BindView(R.id.zoom_out)
+  ImageView mZoomOut;
+
+
+  MapHelper mapHelper;
+
+
   public GameMapShowController(AppCompatActivity activity, View view) {
     super(activity, view);
+    ButterKnife.bind(this,view);
   }
 
+  private void init(){
+    mapHelper = new MapHelper(getActivity(), mapView);
+  }
+
+
+
+
+  @OnClick(R.id.zoom_in)
+  void zoomIn() {
+    if (mapHelper.mapZoomIn()) {
+      mZoomOut.setClickable(true);
+    } else {
+      mZoomIn.setClickable(false);
+    }
+  }
+
+  @OnClick(R.id.zoom_out)
+  void zoomOut() {
+    if (mapHelper.mapZoomOut()) {
+      mZoomIn.setClickable(true);
+    } else {
+      mZoomOut.setClickable(false);
+    }
+  }
 
 }
